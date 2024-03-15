10cd3e953b90ae4244681150ccf5f189184f15b9
andrewmalley725 <andrew.alley99@gmail.com>
2024-03-15 12:08:33 -0600

    login page

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
-export default function App() {
+const apiUri = 'https://finance-app-flask-afh4ecdnpq-uc.a.run.app';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [msg, setMsg] = useState('');
+
+  const handleLogin = () => {
+    console.log('Logging in with:', username, password);
+
+    const body = {
+      username: username, 
+      password: password
+    }
+
+    axios.post(`${apiUri}/authenticate`, body).then(async (res) => {
+      setUsername('');
+      setPassword('');
+      if (res.data.status !== 'successfully logged in'){
+        setMsg(res.data.status);
+      }
+      else{
+        await AsyncStorage.setItem('apiKey', res.data.apiKey);
+        await AsyncStorage.setItem('user', JSON.stringify(res.data.user));
+      }
+      console.log(res.data)
+    })
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.logo}>Expense Tracker</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Username"
+          placeholderTextColor="#003f5c"
+          onChangeText={text => setUsername(text)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={text => setPassword(text)}
+        />
+      </View>
+      <View>
+        <Text style={styles.errorMsg}>{msg}</Text>
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003f5c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: '#fb5b5a',
+    marginBottom: 40,
+  },
+  inputView: {
+    width: '80%',
+    backgroundColor: '#465881',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: 'white',
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#fb5b5a',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: 'white',
+  },
+  errorMsg: {
+    color: 'red'
+  }
 });
+
+export default App;
